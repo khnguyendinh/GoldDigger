449bf80d8a8ec79f669864a71a77db5b74b9445f
khoand <khnguyendinh@gmail.com>
2017-01-04 16:10:23 +0700

    kết thúc bài 3

---
+++ GoldDigger/Assets/Scripts/Config.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Config : MonoBehaviour {
+    public const string TAG_GOLD = "gold";
+    public const string TAG_BOOM = "boom";
+}
--- GoldDigger/Assets/Scripts/Pod.cs
+++ GoldDigger/Assets/Scripts/Pod.cs
@@ -11,8 +11,42 @@ public class Pod : MonoBehaviour {
     }
     PodState podState = PodState.ROTATION;
     private int _angle;
+    private float _slowDown;
+    #region Serialize
+    [SerializeField]
     private int _rotateSpeed = 2;
-    void Awake() { 
+    [SerializeField]
+    private float _speed;
+    public int _dollar,_sumDollar;
+    private Vector3 _original;
+    private Transform _Rod;
+    private bool _flagged;
+    private GUIText _score;
+    #endregion
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_flagged)
+            return;
+        _flagged = true;
+        _Rod = collision.transform;
+        switch (collision.tag)
+        {
+            case Config.TAG_BOOM:
+                //_Rod.tag = this.tag;
+                _Rod.GetComponent<Boom>().Bang(_Rod.position);
+                break;
+            case Config.TAG_GOLD:
+                _Rod.SetParent(transform);
+                break;
+        }
+        _dollar = _Rod.GetComponent<Rod>().dollar;
+     //   _Rod.transform.rotation = Quaternion.Euler(0, 0, 0);
+        _slowDown = _Rod.GetComponent<Rod>().slowDown;
+        podState = PodState.REWIND;
+    }
+    void Awake() {
+        _original = transform.position;
+        _score = Camera.main.GetComponent<GUIText>();
     }
 	void Update () {
         switch (podState)
@@ -23,7 +57,7 @@ public class Pod : MonoBehaviour {
                     podState = PodState.SHOOT;
                 }
                 _angle += _rotateSpeed;
-                if (_angle > 80 || _angle < -80)
+                if (_angle > 70 || _angle < -70)
                 {
                     _rotateSpeed *= -1;
                 }
@@ -31,9 +65,34 @@ public class Pod : MonoBehaviour {
                 transform.rotation = Quaternion.AngleAxis(_angle,Vector3.forward);
                 break;
             case PodState.SHOOT:
+
+                transform.Translate(Vector3.down * _speed * Time.deltaTime);
+                if(Mathf.Abs(transform.position.x) > 7 || transform.position.y < -4)
+                {
+                    podState = PodState.REWIND;
+                }
                 break;
             case PodState.REWIND:
-                break;
+                transform.Translate(Vector3.up * (_speed - _slowDown)* Time.deltaTime);
+                if (Mathf.Floor(transform.position.x) == Mathf.Floor(_original.x) &&
+                    Mathf.Floor(transform.position.y) == Mathf.Floor(_original.y))
+                {
+                    if(_Rod != null)
+                    {
+                        _slowDown = 0;
+                        _flagged = false;
+                        addDollar(_dollar);
+                        Destroy(_Rod.gameObject);
+                    }
+                    transform.position = _original;
+                    podState = PodState.ROTATION;
+                }
+                    break;
         }
 	}
+    void addDollar(int addDollar)
+    {
+        _sumDollar += addDollar;
+        _score.text = string.Format("$ {0}", _sumDollar);
+    }
 }
